df2ae26413a17d33526200861a1d57fe51d46028
GenisWon <bbgenis11@gmail.com>
2019-03-19 13:15:31 -0400

    Initial Commit - Added landing, sign up, and log in screens

---
+++ app/src/main/java/com/example/savememoney/landing.java
@@ -0,0 +1,36 @@
+package com.example.savememoney;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        Button btnSignUp = (Button)findViewById(R.id.button_signup);
+        Button btnLogIn = (Button)findViewById(R.id.button_login);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(landing.this, SignUpActivity.class));
+            }
+        });
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(landing.this, LoginActivity.class));
+            }
+        });
+    }
+
+
+
+}
